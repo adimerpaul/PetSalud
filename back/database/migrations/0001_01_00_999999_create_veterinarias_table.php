eee1d791b5c95966637c39655d9bc41a0ea7a0c5
adimerpaul <adimer101@gmail.com>
2025-12-22 17:11:23 -0400

    primera version

---
+++ back/database/migrations/0001_01_00_999999_create_veterinarias_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('veterinarias', function (Blueprint $table) {
+            $table->id();
+//            nombre direccion telefono email logo imagen descripcion estado color
+            $table->string('nombre');
+            $table->string('direccion')->nullable();
+            $table->string('telefono')->nullable();
+            $table->string('email')->nullable();
+            $table->string('logo')->nullable();
+            $table->string('imagen')->nullable();
+            $table->text('descripcion')->nullable();
+            $table->string('estado')->default('Activo');
+            $table->string('color')->nullable();
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('veterinarias');
+    }
+};
